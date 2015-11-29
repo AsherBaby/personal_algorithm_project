cc3f83ef7b3a8d1471c20729a93248a5407843c4
Pei Zhao <pxz83@case.edu>
2015-11-29 15:35:00 -0500

    Implement encode method in tiny url module

--- tiny_url/tiny_url.py
+++ tiny_url/tiny_url.py
@@ -4,15 +4,16 @@ class TinyURL:
     def __init__(self):
         self.long_to_short = dict()
         self.short_to_long = dict()
+        self._encode_table = self._generate_encode_table()
 
     def generate(self, long_url):
         """ Generate tiny url """
         if long_url in self.long_to_short:
             return self.long_to_short[long_url]
-        n = self._generate()
-        self.long_to_short[long_url] = n
-        self.short_to_long[n] = long_url
-        return n
+        short_url = self._generate()
+        self.long_to_short[long_url] = short_url
+        self.short_to_long[short_url] = long_url
+        return short_url
 
     def lookup(self, tiny_url):
         """ Return original long url """
@@ -22,6 +23,20 @@ class TinyURL:
     def _generate(self):
         return self._encode(len(self.long_to_short))
 
+    def _generate_encode_table(self):
+        encode_table = [None] * 62
+        for i in range(10):
+            encode_table[i] = chr(ord('0') + i)
+        for i in range(10, 36):
+            encode_table[i] = chr(ord('a') + i - 10)
+        for i in range(36, 62):
+            encode_table[i] = chr(ord('A') + i - 36)
+        return encode_table
+
     def _encode(self, decimal):
         """ [0-9] -> [0-9a-zA-Z] """
-        return decimal  # TODO
+        code = ''
+        while decimal > 0:
+            code += self._encode_table[decimal%62]
+            decimal //= 62
+        return code[::-1] if code else '0'  #!
--- tiny_url/unit_test_tiny_url.py
+++ tiny_url/unit_test_tiny_url.py
@@ -6,7 +6,7 @@ class TestTinyURL(unittest.TestCase):
     def test_generate(self):
         tiny_url = TinyURL()
         self.assertEqual(
-            tiny_url.generate('aaa'), 0)
+            tiny_url.generate('aaa'), '0')
 
     def test_lookup(self):
         tiny_url = TinyURL()
@@ -14,6 +14,15 @@ class TestTinyURL(unittest.TestCase):
             tiny_url.lookup(tiny_url.generate('bbb')), 'bbb')
         self.assertEqual(
             tiny_url.lookup('ccc'), None)
+
+    def test_encode(self):
+        tiny_url = TinyURL()
+        self.assertEqual(
+            tiny_url._encode(0), '0')
+        self.assertEqual(
+            tiny_url._encode(10), 'a')
+        self.assertEqual(
+            tiny_url._encode(62), '10')
 
 if __name__ == '__main__':
     unittest.main()
