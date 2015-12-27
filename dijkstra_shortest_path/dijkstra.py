6442b657cdb2053abd6185ca09e6973b657cdbd4
Pei Zhao <pei.zhao.cs@gmail.com>
2015-12-27 16:38:44 -0500

    Rewrite dijkstra algorithm into an object

--- dijkstra_shortest_path/dijkstra.py
+++ dijkstra_shortest_path/dijkstra.py
@@ -1,47 +1,56 @@
 """Dijkstra's shortest path algorithm"""
 from hash_heap import HashHeap
 from graph import Graph
-# import ipdb; ipdb.set_trace()
 
 
-def dijkstra(graph, source):
-    """Dijkstra
+class ShortestDist:
 
-    This method computes shortest distance and path for each node
-    from the source node, and save results in each node's distance
-    attribute.
-    return None
-    """
-    reachable = preprocessing(graph, source)
-    for id in reachable:
-        graph[id].dijkstra_dist = float('inf')
-    source.dijkstra_dist = 0
-    unknown = HashHeap(key=lambda x, y: x.dijkstra_dist < y.dijkstra_dist)
-    for node_id in reachable:
-        unknown.add(graph[node_id])  # add to unknown
-    visited = set()
+    def __init__(self, graph, source_id):
+        self.graph = graph
+        self.source = graph[source_id]
+        self.dijkstra(self.graph, self.source)
 
-    while unknown:
-        node = unknown.pop()
-        visited.add(node.id)  # add to known
+    def dist(self, node_id):
+        return self.graph[node_id].dijkstra_dist
+
+    def dijkstra(self, graph, source):
+        """Dijkstra
+
+        This method computes shortest distance and path for each node
+        from the source node, and save results in each node's distance
+        attribute.
+        return None
+        """
+        reachable = self.preprocessing(graph, source)
+        for id in reachable:
+            graph[id].dijkstra_dist = float('inf')
+        source.dijkstra_dist = 0
+        unknown = HashHeap(key=lambda x, y: x.dijkstra_dist < y.dijkstra_dist)
+        for node_id in reachable:
+            unknown.add(graph[node_id])  # add to unknown
+        visited = set()
+
+        while unknown:
+            node = unknown.pop()
+            visited.add(node.id)  # add to known
+            for edge in node.edges:
+                adj = graph[edge.dest]
+                if (edge.dest not in visited and
+                        node.dijkstra_dist + edge.dist < adj.dijkstra_dist):
+                    adj.dijkstra_dist = node.dijkstra_dist + edge.dist
+                    unknown.update(adj)  # method only in hash heap
+
+    def preprocessing(self, graph, source):
+        """
+        Return reachable nodes id as a list
+        """
+        marked = [False] * graph.n
+        self.dfs(graph, source, marked)
+        return [i for i, item in enumerate(marked) if item]
+
+    def dfs(self, graph, node, marked):
+        marked[node.id] = True
         for edge in node.edges:
             adj = graph[edge.dest]
-            if (edge.dest not in visited and
-                    node.dijkstra_dist + edge.dist < adj.dijkstra_dist):
-                adj.dijkstra_dist = node.dijkstra_dist + edge.dist
-                unknown.update(adj)  # method only in hash heap
-
-def preprocessing(graph, source):
-    """
-    Return reachable nodes id as a list
-    """
-    marked = [False] * graph.n
-    dfs(graph, source, marked)
-    return [i for i, item in enumerate(marked) if item]
-
-def dfs(graph, node, marked):
-    marked[node.id] = True
-    for edge in node.edges:
-        adj = graph[edge.dest]
-        if not marked[adj.id]:
-            dfs(graph, adj, marked)
+            if not marked[adj.id]:
+                self.dfs(graph, adj, marked)
--- dijkstra_shortest_path/unit_test_dijkstra.py
+++ dijkstra_shortest_path/unit_test_dijkstra.py
@@ -1,30 +1,30 @@
 import unittest
-from dijkstra import dijkstra
+from dijkstra import ShortestDist
 from graph import Graph
 
 class TestDijkstra(unittest.TestCase):
 
-    def test_of_dijkstra_small_dataset(self):
+    def test_of_dijkstra_tiny_dataset(self):
         graph = Graph(filename='tinyG.txt')
-        dijkstra(graph, graph[1])
-        self.assertEqual(graph[4].dijkstra_dist, 2)
+        shortest_dist = ShortestDist(graph, 1)
+        self.assertEqual(shortest_dist.dist(4), 2)
 
     def test_of_dijkstra_medium_dataset(self):
         graph = Graph(filename='mediumG.txt')
-        dijkstra(graph, graph[13])
-        self.assertEqual(graph[5].dijkstra_dist, 26)
+        shortest_dist = ShortestDist(graph, 13)
+        self.assertEqual(shortest_dist.dist(5), 26)
 
     def test_of_dijkstra_large_dataset(self):
         graph = Graph(filename='largeG.txt')
-        dijkstra(graph, graph[28])
-        self.assertEqual(graph[6].dijkstra_dist, 9)
+        shortest_dist = ShortestDist(graph, 28)
+        self.assertEqual(shortest_dist.dist(6), 9)
 
-    def test_of_dijkstra_extra_dataset(self):
+    def test_of_dijkstra_giant_dataset(self):
         graph = Graph(filename='giantG.txt')
         # Test data hornored verified by Coursera.org
-        dijkstra(graph, graph[1])
+        shortest_dist = ShortestDist(graph, 1)
         test_nodes_id = [7,37,59,82,99,115,133,165,188,197]
-        distances = [graph[id].dijkstra_dist for id in test_nodes_id]
+        distances = [shortest_dist.dist(id) for id in test_nodes_id]
         self.assertEqual(
             distances,
             [2599,2610,2947,2052,2367,2399,2029,2442,2505,3068])
