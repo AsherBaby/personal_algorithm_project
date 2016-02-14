955569f092eb2096d6f175a5c60065f6f98749c2
Pei Zhao <pei.zhao.cs@gmail.com>
2016-02-14 14:57:04 -0800

    Bug fixes in Sudoku Solver
    Thanks, Ji.

--- graphs/backtracking/sudoku/sudoku.py
+++ graphs/backtracking/sudoku/sudoku.py
@@ -1,41 +1,58 @@
 """
 Sudoku solver
 """
-
+#import ipdb; ipdb.set_trace()
 class Sudoku:
 
-    def __init__(self, board):
-        self.board = board
+    def __init__(self, grid):
+        self.grid = grid
 
     def solve(self):
-        self.dfs(0, 0)
-        return self.board
+        return self.dfs(0, 0)
 
     def dfs(self, i, j):
-        if i == 9 and j == 0:
+        if self.is_last(i, j):
             return True
-        for x in range(1, 10):
-            if (x not in self.board[i] and
-                x not in [row[j] for row in self.board] and
-                x not in self.square(i, j)):
-                self.board[i][j] = x
+
+        grid = self.grid
+        if grid[i][j]:
+            #!! pre-filled
+            if self.valid(i, j):
                 n_i, n_j = self.next(i, j)
                 if self.dfs(n_i, n_j):
                     return True
-        self.board[i][j] = None
+            return False
+
+        for x in range(1, 10):
+            grid[i][j] = x
+            if self.valid(i, j):
+                n_i, n_j = self.next(i, j)
+                if self.dfs(n_i, n_j):
+                    return True
+        #!!
+        grid[i][j] = 0
+        return False
+
+    def valid(self, i, j):
+        x = self.grid[i][j]
+        #!!
+        return (
+            self.grid[i].count(x) < 2 and
+            [row[j] for row in self.grid].count(x) < 2 and
+            self.square(i, j).count(x) < 2)
 
     def square(self, i, j):
-        i /= 3
-        j /= 3
+        i //= 3
+        j //= 3
         ret = []
         for x in range(i*3, i*3+3):
             for y in range(j*3, j*3+3):
-                if self.board[x][y]:
-                    ret.append(self.board[x][y])
+                if self.grid[x][y]:
+                    ret.append(self.grid[x][y])
         return ret
 
+    def is_last(self, i, j):
+        return i == 9 and j == 0
+
     def next(self, i, j):
-        if j < 8:
-            return i, j+1
-        else:
-            return i+1, 0
+        return (i, j+1) if j < 8 else (i+1, 0)
--- graphs/backtracking/sudoku/unit_test_sukodu.py
+++ graphs/backtracking/sudoku/unit_test_sukodu.py
@@ -3,30 +3,30 @@ from sudoku import Sudoku
 
 class TestSolveSudoku(unittest.TestCase):
 
-    def test_of_empty_board(self):
+    def test_of_easy_board(self):
         board = [
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-            [None, None, None, None, None, None, None, None, None,],
-        ]
+            [4, 0, 0, 6, 0, 2, 0, 8, 1,],
+            [2, 5, 0, 0, 8, 7, 0, 0, 0,],
+            [0, 0, 6, 0, 0, 0, 2, 4, 0,],
+            [0, 0, 0, 0, 0, 0, 3, 0, 0,],
+            [0, 2, 7, 3, 5, 4, 8, 9, 0,],
+            [0, 0, 5, 0, 0, 0, 0, 0, 0,],
+            [0, 8, 2, 1, 0, 0, 4, 0, 0,],
+            [0, 0, 0, 2, 7, 0, 0, 1, 8,],
+            [6, 1, 0, 8, 0, 9, 0, 0, 5,]]
         s = Sudoku(board)
+        s.solve()
         self.assertEqual(
-            s.solve(),
-            [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-             [4, 5, 6, 7, 8, 9, 1, 2, 3],
-             [7, 8, 9, 1, 2, 3, 4, 5, 6],
-             [2, 1, 4, 3, 6, 5, 8, 9, 7],
-             [3, 6, 5, 8, 9, 7, 2, 1, 4],
-             [8, 9, 7, 2, 1, 4, 3, 6, 5],
-             [5, 3, 1, 6, 4, 2, 9, 7, 8],
-             [6, 4, 2, 9, 7, 8, 5, 3, 1],
-             [9, 7, 8, 5, 3, 1, 6, 4, 2]])
+            s.grid,
+            [[4, 7, 9, 6, 3, 2, 5, 8, 1],
+             [2, 5, 1, 4, 8, 7, 9, 6, 3],
+             [8, 3, 6, 5, 9, 1, 2, 4, 7],
+             [9, 4, 8, 7, 1, 6, 3, 5, 2],
+             [1, 2, 7, 3, 5, 4, 8, 9, 6],
+             [3, 6, 5, 9, 2, 8, 1, 7, 4],
+             [7, 8, 2, 1, 6, 5, 4, 3, 9],
+             [5, 9, 4, 2, 7, 3, 6, 1, 8],
+             [6, 1, 3, 8, 4, 9, 7, 2, 5]])
 
 
 if __name__ == '__main__':
