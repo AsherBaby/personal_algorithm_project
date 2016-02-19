ee72cafa78e5c2aead280cd80b8652cecc521439
Pei Zhao <pei.zhao.cs@gmail.com>
2016-02-19 12:25:38 -0800

    Improve code of max num of non-overlapping intervals

--- arrays/max_non_overlapping_intervals.py
+++ arrays/max_non_overlapping_intervals.py
@@ -10,19 +10,12 @@ class Interval:
         self.s = s
         self.f = f
 
-def max_non_overlapping_intervals(a):
-    # type: (list) -> int
-    """
-    >>> a = [Interval(0,1), Interval(2,4), Interval(1,3)]
-    >>> max_non_overlapping_intervals(a)
-    2
-    """
-    a.sort(key=lambda x: x.f)
-    n = len(a)
+def max_non_overlapping_intervals(A):
+    A.sort(key=lambda x: x.f)
     count = 0
     f = 0
-    for interval in a:
-        if interval.s >= f:
+    for interval in A:
+        if f <= interval.s:
             count += 1
             f = interval.f
     return count
