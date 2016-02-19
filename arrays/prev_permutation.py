ec20099766d084acb468b335af12ac7c2c0a7195
Pei Zhao <pei.zhao.cs@gmail.com>
2016-02-19 15:29:14 -0800

    Previous permutation is added, next permutation is improved.

--- arrays/prev_permutation.py
+++ arrays/prev_permutation.py
@@ -1,21 +1,17 @@
-"""
-Compute next permutation of P
-
-Run doctest: python -m doctest -v next_permutation.py
-"""
-
-def next_permutation(p):
+def prev_premutation(p):
     n = len(p)
     found = False
     for i in range(n-2, -1, -1):
-        if p[i] < p[i+1]:
+        if p[i] > p[i+1]:
             found = True
             break
     if not found:
         return reversed(p)
     for j in range(n-1, i, -1):
-        if p[j] > p[i]:
+        if p[j] < p[i]:
             break
     p[i], p[j] = p[j], p[i]
     p[i+1:] = reversed(p[i+1:])
     return p
+
+print(list(prev_premutation([1])))
