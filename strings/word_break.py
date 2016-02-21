39c4ed8e3ee97a84417ce83ba2be18185cd5f4d7
Pei Zhao <pei.zhao.cs@gmail.com>
2016-02-20 22:08:00 -0800

    Word break is added

--- strings/word_break.py
+++ strings/word_break.py
@@ -1,49 +1,18 @@
-"""
-input:
-    s = 'iamhappy'
-    dictionary = {'i', 'am', 'happy', 'afternoon', 'after', 'noon'}
-output version 1:
-    ['i', 'am', 'happy']
-output version 2:
-    True
+ans = []
+dict = {'i', 'am', 'happy', 'this', 'afternoon', 'after', 'noon'}
 
-"""
+def word_break(s):
+    get_word(s, [])
 
-def word_break1(s, d):
-    # type: (str, set) => list
-    """
-    >>> s = 'afternoon'
-    >>> d = {'i', 'am', 'happy', 'afternoon', 'after', 'noon'}
-    >>> word_break1(s, d)
-    [['after', 'noon'], ['afternoon']]
-    """
-    ans = []
-    get_word(s, d, ans, [])
-    return ans
-
-def get_word(s, d, ans, words):
+def get_word(s, seg):
     if not s:
-        ans.append(words[:])
+        ans.append(seg[:])
         return
     for i in range(len(s)):
-        if s[:i+1] in d:
-            words.append(s[:i+1])
-            get_word(s[i+1:], d, ans, words)
-            words.pop()
+        if s[:i+1] in dict:
+            seg.append(s[:i+1])
+            get_word(s[i+1:], seg)
+            seg.pop()
 
-def word_break2(s, d):
-    # type: (str, set) => bool
-    """
-    >>> s = 'afternoon'
-    >>> d = {'i', 'am', 'happy', 'afternoon', 'after', 'noon'}
-    >>> word_break2(s, d)
-    True
-    """
-    n = len(s)
-    dp = [False] * (n+1)
-    # dp[i] -> string[:i] is breakable
-    dp[0] = True
-    for i in range(1, n+1):
-        for j in range(i):
-            dp[i] = dp[i] or (dp[j] and s[j:i] in d)
-    return dp[n]
+word_break('iamhappythisafternoon')
+print(ans)
