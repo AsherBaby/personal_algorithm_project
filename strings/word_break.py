d0ad40688aa8365486bf46740b61b8a7ab58ccb8
Pei Zhao <pei.zhao.cs@gmail.com>
2016-02-21 21:51:19 -0800

    Can string be broken into words is added
    One trick to boost the algorithm from O(n^3) to O(n) is to limit the
    search to the length of longest word in dictionary.

--- strings/word_break.py
+++ strings/word_break.py
@@ -14,5 +14,16 @@ def get_word(s, seg):
             get_word(s[i+1:], seg)
             seg.pop()
 
-word_break('iamhappythisafternoon')
-print(ans)
+def word_break2(s):
+    # O(n * w^2) algorithm
+    # where w is the length of longest word in dict
+    w = max([len(x) for x in dict]) if dict else 0
+    n = len(s)
+    can_break = [False] * (n+1)
+    can_break[0] = True
+    for i in range(1, n+1):
+        for j in range(i-1, max(-1, i-1-w), -1):
+            if s[j:i] in dict and can_break[j]:
+                can_break[i] = True
+                break
+    return can_break[n]
