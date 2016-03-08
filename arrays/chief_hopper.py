8b9d887c30989ca6b9f1d47c9e730a747ee2b71b
Pei Zhao <pei.zhao.cs@gmail.com>
2016-03-07 17:53:59 -0800

    Chief hopper problem is added

---
+++ arrays/chief_hopper.py
@@ -0,0 +1,31 @@
+"""
+Chief Hopper Problem
+
+This piece of code is unprecedented for the self-defined
+sequence class and the compute-when-need array.
+"""
+from bisect import bisect_left
+import collections
+
+A = [int(x) for x in input().strip().split()]
+
+class FinalEnergyList(collections.Sequence):
+
+    def __init__(self, f, n):
+        self.f = f
+        self.n = n
+
+    def __len__(self):
+        return n
+
+    def __getitem__(self, x):
+        if not (0 <= x < self.n):
+            raise IndexError
+        return self.f(x)
+
+def f(x):
+    for h in A:
+        x = 2*x - h
+    return x
+
+ans = bisect_left(FinalEnergyList(f, pow(10, 5)), 0)
