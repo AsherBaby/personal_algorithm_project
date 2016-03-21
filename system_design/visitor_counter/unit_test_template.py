deb0c286ceb7fb63dc9cbb547faabbeef648605e
Pei Zhao <pei.zhao.cs@gmail.com>
2016-03-20 18:10:27 -0700

    Visitor Counter is debugged and improved

--- system_design/visitor_counter/unit_test_template.py
+++ system_design/visitor_counter/unit_test_template.py
@@ -1,21 +1,43 @@
 import unittest
-from datetime import datetime, timedelta
-from visitor_counter import VisitorCounter
+from visitor_counter import VisitorCounterByQueue, VisitorCounterByArray
 
-class TestVisitorCounter(unittest.TestCase):
+class TestVisitorCounterByQueue(unittest.TestCase):
 
     def setUp(self):
-        self.counter = VisitorCounter()
+        self.counter = VisitorCounterByQueue()
 
-    def test_of_same_hour(self):
-        self.counter.log(datetime.now()+timedelta(minutes=15))
-        self.counter.log(datetime.now()+timedelta(minutes=15))
-        self.assertEqual(self.counter.get(), 2)
+    def test_of_same_hour_get(self):
+        self.counter.log_hit(1)
+        self.counter.log_hit(1)
+        self.counter.log_hit(2)
+        ans = self.counter.get_hit(4)
+        self.assertEqual(ans, 3)
 
-    def test_of_different_hour(self):
-        self.counter.log(datetime.now()+timedelta(minutes=15))
-        self.counter.log(datetime.now()+timedelta(hours=1, minutes=15))
-        self.assertEqual(self.counter.get(), 1)
+    def test_of_another_hour_get(self):
+        self.counter.log_hit(1)
+        self.counter.log_hit(1)
+        self.counter.log_hit(2)
+        ans = self.counter.get_hit(61)
+        self.assertEqual(ans, 1)
+
+class TestVisitorCounterByArray(unittest.TestCase):
+
+    def setUp(self):
+        self.counter = VisitorCounterByArray()
+
+    def test_of_same_hour_get(self):
+        self.counter.log_hit(1)
+        self.counter.log_hit(1)
+        self.counter.log_hit(2)
+        ans = self.counter.get_hit(4)
+        self.assertEqual(ans, 3)
+
+    def test_of_another_hour_get(self):
+        self.counter.log_hit(1)
+        self.counter.log_hit(1)
+        self.counter.log_hit(2)
+        ans = self.counter.get_hit(61)
+        self.assertEqual(ans, 1)
 
 if __name__ == '__main__':
     unittest.main()
--- system_design/visitor_counter/visitor_counter.py
+++ system_design/visitor_counter/visitor_counter.py
@@ -1,25 +1,60 @@
 """
 Count how many visitors has come to store in an hour with precision to a minute.
 """
-from datetime import datetime, timedelta
+from collections import deque
+from threading import Lock
 
+class VisitorCounterByQueue:
 
-class VisitorCounter:
+    class Hit:
+        def __init__(self, time):
+            self.time = time
+            self.cnt = 1
 
     def __init__(self):
-        self.time_bucket = [[datetime.now(), 0] for _ in range(60)]
-        self.total_count = 0
-        self.start_time = datetime.now()
+        self.queue = deque(maxlen=60)
 
-    def log(self, dt):
-        # dt: datetime
-        idx = (dt - self.start_time) % timedelta(hours=1) // timedelta(minutes=1)
-        if dt - self.time_bucket[idx][0] < timedelta(hours=1):
-            self.time_bucket[idx][1] += 1
-            self.total_count += 1
+    def log_hit(self, time):
+        if self.queue and time == self.queue[-1].time:
+            self.queue[-1].cnt += 1
         else:
-            self.total_count -= self.time_bucket[idx][1] - 1
-            self.time_bucket[idx] = [dt, 1]
+            if len(self.queue) == self.queue.maxlen:
+                queue.popleft()
+            self.queue.append(self.Hit(time))
 
-    def get(self):
-        return self.total_count
+    def get_hit(self, time):
+        while self.queue and time - self.queue[0].time >= 60:
+            self.queue.popleft()
+        ans = 0
+        for hit in self.queue:
+            ans += hit.cnt
+        return ans
+
+class VisitorCounterByArray:
+    """
+    time bucket
+    """
+
+    def __init__(self):
+        self.time_array = list(range(60))
+        self.cnt_array = [0] * 60
+        self.lock = Lock()
+
+    def log_hit(self, time):
+        # write
+        idx = time % 60
+        with self.lock:
+            if time - self.time_array[idx] >= 60:
+                self.time_array[idx] = time
+                self.cnt_array[idx] = 1
+            else:
+                self.cnt_array[idx] += 1
+
+    def get_hit(self, time):
+        # read
+        cnt = 0
+        for i in range(60):
+            with self.lock:
+                if time - self.time_array[i] < 60:
+                    cnt += self.cnt_array[i]
+        return cnt
