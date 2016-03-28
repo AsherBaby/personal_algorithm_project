235736c404452f9471a39049d4f77ae285bd3b44
Pei Zhao <pei.zhao.cs@gmail.com>
2016-03-27 19:54:51 -0700

    Optimized hash heap by using a different hashing mechanism

--- data_structures/hash_heap.py
+++ data_structures/hash_heap.py
@@ -1,30 +1,37 @@
+"""
+Hash heap, a heap data structure that supports O(logN) delete operation.
+
+Hash heap use hash table to map key to index of that key stored in heap array.
+"""
+
+
 class HashHeap:
 
     class Node:
-        def __init__(self, key, idx):
+        def __init__(self, key):
             self.key = key
-            self.idx = idx
             self.num = 1
 
     def __init__(self, type):
-        self.heap = []  # heap: list of Node(key, idx, num)
-        self.hash = dict()  # hash: key -> Node(key, idx, num)
+        self.heap = []  # heap: list of Node(key, num)
+        self.hash = dict()  # hash: key -> idx
         self.type = type
 
     def __bool__(self):
         # test if hash heap is empty
         return bool(self.heap)
 
-    def cmp(self, idx1, idx2):
-        key1 = self.heap[idx1].key
-        key2 = self.heap[idx2].key
+    def cmp(self, k1, k2):
+        key1 = self.heap[k1].key
+        key2 = self.heap[k2].key
         return key1 < key2 if self.type == 'min' else key1 > key2
 
     def swap(self, k1, k2):
         heap = self.heap
+        hash = self.hash
         heap[k1], heap[k2] = heap[k2], heap[k1]  # swap
-        heap[k1].idx = k1  # update
-        heap[k2].idx = k2
+        hash[heap[k1].key] = k1
+        hash[heap[k2].key] = k2
 
     def top(self):
         return self.heap[0].key
@@ -33,21 +40,21 @@ class HashHeap:
         heap = self.heap
         hash = self.hash
         if key in hash:
-            hash[key].num += 1
+            heap[hash[key]].num += 1
         else:
-            node = self.Node(key, len(heap))
+            node = self.Node(key)
             heap.append(node)
-            hash[key] = node
+            hash[key] = len(heap)-1
             self.swim(len(heap)-1)
 
     def delete(self, key):
         heap = self.heap
         hash = self.hash
-        if hash[key].num > 1:
-            hash[key].num -= 1
+        idx = hash[key]
+        if heap[idx].num > 1:
+            heap[idx].num -= 1
         else:
-            idx = hash[key].idx
-            self.swap(idx, -1)
+            self.swap(idx, len(heap)-1)
             del hash[key]
             del heap[-1]
             if idx != len(heap):  #!!
