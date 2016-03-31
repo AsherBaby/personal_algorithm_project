e1106af2cce549c22383a609c1eff7fd2846b04a
Pei Zhao <pei.zhao.cs@gmail.com>
2016-03-30 22:32:43 -0700

    Add test for CCI6-1

--- math/java/src/cci/chap6/BottleOfPills.java
+++ math/java/src/cci/chap6/BottleOfPills.java
@@ -1,13 +1,24 @@
 package cci.chap6;
 
+import java.util.Arrays;
+
 public class BottleOfPills {
 
-    public static double findTheUnique(double[] bottles) {
+    public static int findTheUnique(double[] bottles) {
         double total = 0;
         for (int i = 0; i < 20; i++) {
             total += i * bottles[i];
         }
-        return (total - 190) * 10;
+        return (int)((total - 190) * 10);
+    }
+
+    public static void test() {
+        double[] bottles = new double[20];
+        Arrays.fill(bottles, 1);
+        bottles[5] += 0.1;
+        if (findTheUnique(bottles) != 5) {
+            System.out.println("Error");
+        }
     }
 
 }
